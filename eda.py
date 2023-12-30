52168c35c9c708e73fdf893c6ac41eb32bbf4db9
orkhank <118054233+orkhank@users.noreply.github.com>
2023-12-30 20:31:13 +0300

    add a slider for sample size and organize ui

--- eda.py
+++ eda.py
@@ -5,19 +5,23 @@ from st_pages import add_indentation
 from wordcloud import WordCloud
 from preprocess import Preprocess
 from dataset import Datasets
+import io
 
 
 def explore_data(data: pd.DataFrame):
-    # TODO: add character_count, word_count, sentence_count information for each email and compare them based on ham/spam
-    # Print samples
+    """
+    Function to explore and visualize data.
 
-    data['Character_Count'] = data['Body'].apply(len)
-    data['Word_Count'] = data['Body'].apply(lambda x: len(x.split()))
-    data['Sentence_Count'] = data['Body'].apply(lambda x: len(x.split('.')))
+    Args:
+        data (pd.DataFrame): The input DataFrame containing the data.
 
-
+    Returns:
+        None
+    """
+    
     with st.expander("Samples from the dataset(s)"):
-        st.write(data.sample(5))
+        number_of_samples = st.slider("Number of samples", 0, data.shape[0], 5)
+        st.write(data.sample(number_of_samples, random_state=42))
 
     # Plot pie chart of ham/spam distribution
     with st.expander("Ham/Spam Distribution"):
@@ -33,14 +37,19 @@ def explore_data(data: pd.DataFrame):
             ax.pie(label_counts, labels=[" not spam", "spam"], autopct="%0.2f")
 
             st.pyplot(fig)
+    
+    
+    # Write info about the dataset
+    with st.expander("Dataset Info"):
+        buffer = io.StringIO()
+        data.info(buf=buffer)
+        s = buffer.getvalue()
+        st.text(s)
 
-    # Information about the data
-    import io
+    data['Character_Count'] = data['Body'].apply(len)
+    data['Word_Count'] = data['Body'].apply(lambda x: len(x.split()))
 
-    buffer = io.StringIO()
-    data.info(buf=buffer)
-    s = buffer.getvalue()
-    st.text(s)
+    # Show word clouds
     with st.expander("Word Clouds"):
         spam_wordcloud = WordCloud(width=800, height=400).generate(' '.join(data[data['Label'] == 1]['Body']))
         ham_wordcloud = WordCloud(width=800, height=400).generate(' '.join(data[data['Label'] == 0]['Body']))
@@ -50,10 +59,13 @@ def explore_data(data: pd.DataFrame):
 
         st.write("## Ham Word Cloud")
         st.image(ham_wordcloud.to_image(), caption="Ham Word Cloud", use_column_width=True)
-    # TODO: Show word clouds
 
 
 add_indentation()
+
+st.title("Data Exploration")
+# Explain the purpose of this page
+st.markdown("This page is used to explore the data and visualize it. The tabs below show the raw and preprocessed data. The sidebar can be used to configure the data size.")
 
 raw_data_tab, preprocessed_data_tab = st.tabs(["Raw Data", "Preprocessed Data"])
 
@@ -69,15 +81,12 @@ with preprocessed_data_tab:
     with st.expander("Preprocess Steps"):
         preprocess.get_steps()
 
-    # from sklearn.preprocessing import LabelEncoder
 
-    st.write(data)
     preprocessed_body = pd.DataFrame(
         preprocess.transform(data["Body"]),
         columns=["Body"],
         index=data.index,
     )
-    st.write(preprocessed_body)
     preprocessed_data = pd.concat(
         [
             preprocessed_body,
@@ -85,6 +94,7 @@ with preprocessed_data_tab:
         ],
         axis=1,
     )
+    # from sklearn.preprocessing import LabelEncoder
     # le = LabelEncoder()
     # limited_data["Label"] = le.fit_transform(limited_data["Label"])
     explore_data(preprocessed_data)
